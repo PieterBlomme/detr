77a5f35a160ea987034cd667207b23913148a8df
pieter blomme <pieter.blomme@gmail.com>
2020-08-18 21:03:48 +0200

    Change to parameters

--- main.py
+++ main.py
@@ -16,83 +16,180 @@ from datasets import build_dataset, get_coco_api_from_dataset
 from engine import evaluate, train_one_epoch
 from models import build_model
 
+from dataclasses import dataclass
+from typing import Dict, Optional, Tuple
+from rvai.base.cell import Parameters
+from rvai.types import Float, Integer, String, Boolean, Enum
 
-def get_args_parser():
-    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
-    parser.add_argument('--lr', default=1e-4, type=float)
-    parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=2, type=int)
-    parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--epochs', default=300, type=int)
-    parser.add_argument('--lr_drop', default=200, type=int)
-    parser.add_argument('--clip_max_norm', default=0.1, type=float,
-                        help='gradient clipping max norm')
-
-    # Model parameters
-    parser.add_argument('--frozen_weights', type=str, default=None,
-                        help="Path to the pretrained model. If set, only the mask head will be trained")
-    # * Backbone
-    parser.add_argument('--backbone', default='resnet50', type=str,
-                        help="Name of the convolutional backbone to use")
-    parser.add_argument('--dilation', action='store_true',
-                        help="If true, we replace stride with dilation in the last convolutional block (DC5)")
-    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
-                        help="Type of positional embedding to use on top of the image features")
-
-    # * Transformer
-    parser.add_argument('--enc_layers', default=6, type=int,
-                        help="Number of encoding layers in the transformer")
-    parser.add_argument('--dec_layers', default=6, type=int,
-                        help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=2048, type=int,
-                        help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=256, type=int,
-                        help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0.1, type=float,
-                        help="Dropout applied in the transformer")
-    parser.add_argument('--nheads', default=8, type=int,
-                        help="Number of attention heads inside the transformer's attentions")
-    parser.add_argument('--num_queries', default=100, type=int,
-                        help="Number of query slots")
-    parser.add_argument('--pre_norm', action='store_true')
-
-    # * Segmentation
-    parser.add_argument('--masks', action='store_true',
-                        help="Train segmentation head if the flag is provided")
+# Parameters
+@dataclass
+class DetrParameters(Parameters):
+    # learning
+    lr: Float = Parameters.field(
+        default=Float(1e-4),
+        name="Learning Rate", description="Learning rate (for the head)."
+    )
+    lr_backbone: Float = Parameters.field(
+        default=Float(1e-5),
+        name="Learning Rate Backbone", description="Learning rate (for the backbone)."
+    )
+    batch_size: Integer = Parameters.field(
+        default=Float(2),
+        name="Learning Rate Backbone", description="Learning rate (for the backbone)."
+    )
+    weight_decay: Float = Parameters.field(
+        default=Float(1e-4),
+        name="Weight Decay", description="Weight decay"
+    )
+    epochs: Integer = Parameters.field(
+        default=Integer(300),
+        name="Epochs", description="Epochs"
+    )
+    lr_drop: Integer = Parameters.field(
+        default=Integer(200),
+        name="Learning Rate Drop", description="Learning Rate Drop"
+    )
+    clip_max_norm: Float = Parameters.field(
+        default=Float(0.1),
+        name="Clip Max Norm", description="Gradient clipping max norm"
+    )
+    frozen_weights: Optional[String] = Parameters.field(
+        default=None,
+        name="Frozen weights", description="Path to the pretrained model. If set, only the mask head will be trained"
+    )
+    # model
+    backbone: String = Parameters.field(
+        default=String("resnet50"),
+        name="Backbone", description="Name of the convolutional backbone to us"
+    )
+    dilation: Boolean = Parameters.field(
+        default=Boolean(False),
+        name="Dilation", description="If true, we replace stride with dilation in the last convolutional block (DC5)"
+    )
+    position_embedding: Enum[String] = Parameters.field(
+        default=Enum[String](
+            "sine", "learned", selected="sine"
+        ),
+        name="Poistion Embedding",
+        description="Type of positional embedding to use on top of the image features"
+    )
+    # Transformer
+    enc_layers: Integer = Parameters.field(
+        default=Integer(6),
+        name="Encoding layers", description="Number of encoding layers in the transformer"
+    )
+    dec_layers: Integer = Parameters.field(
+        default=Integer(6),
+        name="Decoding layers", description="Number of decoding layers in the transformer"
+    )
+    dim_feedforward: Integer = Parameters.field(
+        default=Integer(2048),
+        name="Dimensions feedforward", description="Intermediate size of the feedforward layers in the transformer blocks"
+    )
+    hidden_dim: Integer = Parameters.field(
+        default=Integer(256),
+        name="Hidden Dimension", description="Size of the embeddings (dimension of the transformer)"
+    )
+    dropout: Float = Parameters.field(
+        default=Float(0.1),
+        name="Dropout", description="Dropout applied in the transformer"
+    )
+    nheads: Integer = Parameters.field(
+        default=Integer(8),
+        name="Number of heads", description="Number of attention heads inside the transformer's attentions"
+    )
+    num_queries: Integer = Parameters.field(
+        default=Integer(100),
+        name="Number of queries", description="Number of query slots"
+    )
+    pre_norm: Boolean = Parameters.field(
+        default=Boolean(False),
+        name="Pre norm", description="Pre norm"
+    )
+    # Segmentation
+    masks: Boolean = Parameters.field(
+        default=Boolean(False),
+        name="Masks", description="Train segmentation head if the flag is provided"
+    )
 
     # Loss
-    parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false',
-                        help="Disables auxiliary decoding losses (loss at each layer)")
-    # * Matcher
-    parser.add_argument('--set_cost_class', default=1, type=float,
-                        help="Class coefficient in the matching cost")
-    parser.add_argument('--set_cost_bbox', default=5, type=float,
-                        help="L1 box coefficient in the matching cost")
-    parser.add_argument('--set_cost_giou', default=2, type=float,
-                        help="giou box coefficient in the matching cost")
-    # * Loss coefficients
-    parser.add_argument('--mask_loss_coef', default=1, type=float)
-    parser.add_argument('--dice_loss_coef', default=1, type=float)
-    parser.add_argument('--bbox_loss_coef', default=5, type=float)
-    parser.add_argument('--giou_loss_coef', default=2, type=float)
-    parser.add_argument('--eos_coef', default=0.1, type=float,
-                        help="Relative classification weight of the no-object class")
+    aux_loss: Boolean = Parameters.field(
+        default=Boolean(True),
+        name="Auxiliary loss", description="Enables auxiliary decoding losses (loss at each layer)"
+    )
+    # Matcher
+    set_cost_class: Float = Parameters.field(
+        default=Float(1.0),
+        name="Set cost class", description="Class coefficient in the matching cost"
+    )
+    set_cost_bbox: Float = Parameters.field(
+        default=Float(5.0),
+        name="Set cost bounding box", description="L1 box coefficient in the matching cost"
+    )
+    set_cost_giou: Float = Parameters.field(
+        default=Float(2.0),
+        name="Set cost GIOU", description="giou box coefficient in the matching cost"
+    )
+
+    # Loss coefficients
+    mask_loss_coef: Float = Parameters.field(
+        default=Float(1.0),
+        name="Mask loss coefficient", description="Mask loss coefficient"
+    )
+    dice_loss_coef: Float = Parameters.field(
+        default=Float(1.0),
+        name="Dice loss coefficient", description="Dice loss coefficient"
+    )
+    bbox_loss_coef: Float = Parameters.field(
+        default=Float(5.0),
+        name="Bounding box loss coefficient", description="Bounding box loss coefficient"
+    )
+    giou_loss_coef: Float = Parameters.field(
+        default=Float(2.0),
+        name="GIOU loss coefficient", description="GIOU loss coefficientt"
+    )
+    eos_coef: Float = Parameters.field(
+        default=Float(0.1),
+        name="Eos coefficient", description="Relative classification weight of the no-object class"
+    )
 
     # dataset parameters
-    parser.add_argument('--dataset_file', default='coco')
-    parser.add_argument('--coco_path', type=str)
-    parser.add_argument('--coco_panoptic_path', type=str)
-    parser.add_argument('--remove_difficult', action='store_true')
-
-    parser.add_argument('--output_dir', default='',
-                        help='path where to save, empty for no saving')
-    parser.add_argument('--seed', default=42, type=int)
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='start epoch')
-    parser.add_argument('--eval', action='store_true')
-    return parser
-
+    dataset_file: String = Parameters.field(
+        default=String("coco"),
+        name="dataset_file", description="dataset_file"
+    )
+    coco_path: String = Parameters.field(
+        default=String(""),
+        name="coco_path", description="coco_path"
+    )
+    coco_panoptic_path: Optional[String] = Parameters.field(
+        default=None,
+        name="coco_panoptic_path", description="coco_panoptic_path"
+    )
+    remove_difficult: Boolean = Parameters.field(
+        default=Boolean(False),
+        name="remove_difficult", description="remove_difficult"
+    )
+    output_dir: String = Parameters.field(
+        default=String(""),
+        name="output_dir", description='path where to save, empty for no saving'
+    )
+    seed: Integer = Parameters.field(
+        default=Integer(42),
+        name="seed", description="seed"
+    )
+    resume: String = Parameters.field(
+        default=String("https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth"),
+        name="resume", description='resume from checkpoint'
+    )
+    start_epoch: Integer = Parameters.field(
+        default=Integer(0),
+        name="start_epoch", description="start_epoch"
+    )
+    eval: Boolean = Parameters.field(
+        default=Boolean(False),
+        name="eval", description="eval"
+    )
 
 def main(args):
     if args.frozen_weights is not None:
@@ -221,8 +318,8 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
-    args = parser.parse_args()
+    #args = DetrParameters(coco_path="/home/jovyan/work/coco")#train
+    args = DetrParameters(aux_loss=False, eval=True, resume="https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth", coco_path="/home/jovyan/work/coco")#eval
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
